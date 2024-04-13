69840740208a5233078be04c9e6826e316c8b01a
mswailem <mswailem@vt.edu>
2024-04-13 19:47:30 -0400

    Nothing Major

--- mean_field/src/testing.cpp
+++ mean_field/src/testing.cpp
@@ -7,18 +7,9 @@
 #include <gsl/gsl_complex.h>
 #include <gsl/gsl_complex_math.h>
 
+
+// NOTE: Currently testing the phase plot of k1 vs alpha, will end up refactoring this better into the code 
+
 int main (int argc, char *argv[]) {
-	DESolver solver(1, 1, 0.22, 0, 1, 1, 1.1, 1.1, 0);
-	solver.initialize();
-	double dt = 0.01;
-	std::cout << solver.get_y()[0] << " " << solver.get_y()[1] << std::endl;
-	solver.solve(0, 1, dt);
-	std::cout << solver.get_y()[0] << " " << solver.get_y()[1] << std::endl;
-	solver.solve(0, 1, dt);
-	std::cout << solver.get_y()[0] << " " << solver.get_y()[1] << std::endl;
-	DESolver solver2(1, 1, 0.22, 0, 1, 1, 1.1, 1.1, 0);
-	std::cout << solver2.get_y()[0] << " " << solver2.get_y()[1] << std::endl;
-	solver2.solve(0, 2, dt);
-	std::cout << solver2.get_y()[0] << " " << solver2.get_y()[1] << std::endl;
 	return 0;
 }
